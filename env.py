c4c2ec9c4beb0fbfd856951ee5f88a71071d3c44
Geza Csosz <geza.csosz@gmail.com>
2024-12-19 15:20:06 +0000

    remove env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:LtFd5baWv4Zo@ep-blue-hall-a2wxh69d.eu-central-1.aws.neon.tech/wrist_eel_dash_28389")
-
-os.environ.setdefault("SECRET_KEY", "<83dF=24zhadsehz32rER-=7sdhgasdhg-!!ERE!!++45fr5>")
