8f7fbec6bf89760767a9caf3908df54b911ebb22
Geza Csosz <geza.csosz@gmail.com>
2024-12-19 13:04:28 +0000

    deployment settings

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "<your-database-URL>")
+    "DATABASE_URL", "postgresql://neondb_owner:LtFd5baWv4Zo@ep-blue-hall-a2wxh69d.eu-central-1.aws.neon.tech/wrist_eel_dash_28389")
