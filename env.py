e0873f3fcc110b45c281a41e807183d4e5ae0cf3
Geza Csosz <geza.csosz@gmail.com>
2024-12-19 13:27:22 +0000

    gitignore

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:LtFd5baWv4Zo@ep-blue-hall-a2wxh69d.eu-central-1.aws.neon.tech/wrist_eel_dash_28389")
+
+os.environ.setdefault("SECRET_KEY", "<83dF=24zhadsehz32rERERE!!++45fr5>")
