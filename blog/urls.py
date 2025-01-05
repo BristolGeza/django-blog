aea0c1f27cd03ffe4bd9017b5d0184ce82e8953e
Geza Csosz <geza.csosz@gmail.com>
2025-01-05 20:51:11 +0000

    add gitignore

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.PostList.as_view(), name='home'),
+    #path("<int:event_id>/", views.event_detail,
+         #name="event_detail")
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    
+    
+]
+
+
+
