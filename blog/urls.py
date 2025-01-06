02cfbaf443fb55439bab299e854a23d0e221c12f
Geza Csosz <geza.csosz@gmail.com>
2025-01-06 15:22:30 +0000

    added forms

--- blog/urls.py
+++ blog/urls.py
@@ -2,13 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name='home'),
-    #path("<int:event_id>/", views.event_detail,
-         #name="event_detail")
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
-    
-    
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name="post_detail"),
 ]
-
-
-
